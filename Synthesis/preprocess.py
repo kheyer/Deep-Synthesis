913a0584dbaa7d6b26d3a5605b3c1a8c83299ab4
Ubuntu <ubuntu@ip-172-31-49-211.us-west-2.compute.internal>
2019-09-18 23:56:39 +0000

    added full preprocessing pipeline for cleaning, canonicalizing and tokenizing an input SMILES

--- Synthesis/preprocess.py
+++ Synthesis/preprocess.py
@@ -1,6 +1,34 @@
 from rdkit import Chem
 from rdkit.Chem import Draw
 
+def preprocess(smiles):
+    # Function to preprocess a single SMILES text string
+
+    # Loading from a file may add a '\n' or ' ' to the end of a SMILES string
+    smiles = smiles.strip('\n')
+    smiles = smiles.strip(' ')
+
+    # if spaces are present, they are joined
+    # this makes the processing pipeline robust to inputs that are already tokenized,
+    # partially tokenized, tokenized with a different method or using space delimiting
+    # to denote reactants from reagents
+    if ' ' in smiles:
+        smiles = ''.join(smiles.split(' '))
+
+    # sometimes a '>' character is used to denote reactants from reagents
+    # this convention is not supported by RDKit, and must be converted to the traditional
+    # '.' delimiter
+    if '>' in smiles:
+        smiles = smiles.replace('>', '.')
+
+    smiles = canonicalize_smiles(smiles, remove_stereo=True)
+        
+    return smiles
+
+def tokenize(smiles):
+    # tokenizes SMILES string by character
+    return ' '.join([i for i in smiles])
+
 def canonicalize_smiles(smiles, remove_stereo=True):
     # Converts input SMILES to their canonical form
     # Has the option to remove stereochemistry from SMILES if desired
