16966b54e57207e5b8d471c1d85d9200af54a608
Ubuntu <ubuntu@ip-172-31-49-211.us-west-2.compute.internal>
2019-09-18 23:37:01 +0000

    created get_data_params function to handle user data input. get_data_params will allow a user to either input a text string or navigate to a file of their choosing. removed base_smile from app_setup to get_data_params

--- Synthesis/utils.py
+++ Synthesis/utils.py
@@ -3,7 +3,28 @@ import os
 
 def app_setup():
     # starter values for prediction type 
-    base_smile = 'O=Cc1cncc(Cl)c1COC1CCCCO1.OCc1c(Cl)cncc1Cl'
     input_options = ['Predict from String', 'Predict from File']
 
-    return (base_smile, input_options) 
+    return input_options
+
+def get_data_params(input_options, prediction_options):
+    # function determines if prediction will be run on a string input by the user
+    # or from a file of SMILES strings
+    if prediction_options == input_options[0]:
+        single_predict = True
+    else:
+        single_predict = False 
+
+    if single_predict:
+        # If single predict, create a text box for user input
+        base_smile = 'O=Cc1cncc(Cl)c1COC1CCCCO1.OCc1c(Cl)cncc1Cl'
+        smile = st.text_input('Input Smile', base_smile)
+        target_smile = st.text_input('Known Target?' , '')
+        # returns single_predict (bool), smile (string), target_smile (string)
+        return single_predict, smile, target_smile
+    else:
+        # If file predict, create text boxes that let users navigate to the prediction file
+        source_filename, target_filename = get_filenames()
+        # returns single_predict (bool), source_filename (string to .txt filename)
+        # target_filename (string to .txt filename if desired, else None)
+        return single_predict, source_filename, target_filename
