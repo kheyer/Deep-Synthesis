dac7f5c39602c9f94796f42f8dde1d12c4c93b57
Ubuntu <ubuntu@ip-172-31-49-211.us-west-2.compute.internal>
2019-09-19 02:38:54 +0000

    updated streamlit app to take user parameters for beam size and top k predictions, load a translation model, and run translations on user data

--- Synthesis/app.py
+++ Synthesis/app.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+import json
 
 from utils import *
 from preprocess import *
@@ -6,6 +7,7 @@ from preprocess import *
 st.title('Deep Synthesis')
 
 input_options = app_setup()
+model_description = json.load(open('configs/model_description.json'))
 
 prediction_option = st.selectbox('Select an Input Format', input_options)
 
@@ -15,3 +17,12 @@ if st.checkbox('Load Data'):
     smile_data = load_data(single_predict, source_param, target_param)
 
     display_data(smile_data)
+
+    beam = int(st.text_input('Beam Width', '10'))
+    n_best = int(st.text_input('Top K Predictions', '5'))
+
+    if st.checkbox('Run Prediction'):
+        placeholder = st.empty()
+        placeholder.text('Translation in Progress')
+        prediction = translate_data(smile_data, beam, n_best, model_description)
+        placeholder.text('Translation Complete')
