1c6aa2cff4b7c5bea9cc20e4b3f2d7baf7e7af03
kheyer <karlheyerv@gmail.com>
2019-09-24 02:19:24 +0000

    created shell script to set up environment

---
+++ buildEnv.sh
@@ -0,0 +1,12 @@
+echo "creating environment"
+conda create -n deep_synthesis
+echo "activating environment"
+source activate deep_synthesis
+echo "installing RDKit"
+conda install rdkit -c rdkit
+echo "installing requirements"
+pip --no-cache-dir install -r requirements.txt
+echo "Cloning OpenNMT"
+git clone https://github.com/kheyer/OpenNMT-py
+echo "Downloading Model"
+python build/download_model.py
