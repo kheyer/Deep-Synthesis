ff39278ebd7846351c8675d83a625594beea70b3
kheyer <karlheyerv@gmail.com>
2019-09-26 17:55:43 +0000

    updated application to better handle different runtime modes (aws vs local) and use unified translation API

--- Synthesis/app.py
+++ Synthesis/app.py
@@ -1,23 +1,42 @@
 import streamlit as st 
 import argparse
+import json
+from utils import *
+from preprocess import *
+from translate_aws import *
 
+# Importing translate also imports OpenNMT.
+# Putting this in a try/except block allows for deploying
+# the app configured to run AWS Lambda predictions
+# without bringing along OpenNMT
+try:
+    from translate import *
+except ModuleNotFoundError:
+    pass
+
+# define runtime specifications on launch
 parser = argparse.ArgumentParser()
 parser.add_argument("runtime", help="Argument to select local or AWS runtimes ['local' or 'AWS']")
 args = parser.parse_args()
 
-import json
-from utils import *
-from preprocess import *
+# Dictionary of configuration files
+config_json_opts = {
+                      'AWS' : 'configs/aws_description.json',
+                      'local' : 'configs/model_description.json'
+                   }
 
-if args.runtime == 'local':
-    from translate import *
-    model_description = json.load(open('configs/model_description.json'))
-elif args.runtime == 'AWS':
-    from translate_aws import *
-    model_description = json.load(open('configs/aws_description.json'))
+runtime = args.runtime 
+model_description = json.load(open(config_json_opts[runtime]))
+
+# Define class to handle translations based on runtime
+if runtime == 'local':
+    translator_class = TranslationModel
+elif runtime == 'AWS':
+    translator_class = LambdaInterface
 else:
     raise ValueError('''Please provide a valid runtime argument. Use 'local' to run predictions locally, or 
                 'AWS' to run predictions on AWS (AWS inference requires permissions)''')
+
 
 st.title('Deep Synthesis')
 
@@ -39,7 +58,7 @@ if st.checkbox('Load Data'):
     if st.checkbox('Run Prediction'):
         placeholder = st.empty()
         placeholder.text('Translation in Progress')
-        prediction = translate_data(smile_data, beam, n_best, True, model_description)
+        prediction = translate_data(smile_data, beam, n_best, True, translator_class, model_description)
         placeholder.text('Translation Complete')
 
         display_prediction(prediction)
